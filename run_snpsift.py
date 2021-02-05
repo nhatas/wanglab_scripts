f1712c1339f19eeed73ca507a9a0617aab5cb0ca
nhatas <53345425+nhatas@users.noreply.github.com>
2021-02-05 16:03:52 -0600

    comment fields

--- run_snpsift.py
+++ run_snpsift.py
@@ -4,8 +4,8 @@ files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
 snpsift_path = "/opt/apps/snpEff/SnpSift.jar"
 
-#fields = 'POS REF ALT "ANN[0].EFFECT" "ANN[0].GENE" "GEN[0].AD" "GEN[0].VAF" "ANN[0].HGVS_P"'
-fields = 'POS REF ALT AF "ANN[0].EFFECT" "ANN[0].GENE" "ANN[0].HGVS_P"'
+#fields = 'POS REF ALT "ANN[0].EFFECT" "ANN[0].GENE" "GEN[0].AD" "GEN[0].VAF" "ANN[0].HGVS_P"' # DeepVariant output
+fields = 'POS REF ALT AF "ANN[0].EFFECT" "ANN[0].GENE" "ANN[0].HGVS_P"' # LoFreq output
 
 for i in files:
     if i.endswith(".ann.vcf"):
